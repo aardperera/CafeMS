03f359dcf23d94aed006b40d2affcfc61831da6a
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2024-06-28 11:50:42 +0530

    create jwtfilter

---
+++ src/main/java/com/inn/cafe/JWT/JwtFilter.java
@@ -0,0 +1,53 @@
+package com.inn.cafe.JWT;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private  JwtUtil jwtUtil;
+
+    @Autowired
+    private CustomerUserDetailsService service;
+
+    Claims claims = null;
+    private String userName = null;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        if (httpServletRequest.getServletPath().matches("/user/login|/user/forgotPassword|/user/signUp|")){
+            filterChain.doFilter(httpServletRequest,httpServletResponse);
+        }
+        else {
+            String authorizationHeader = httpServletRequest.getHeader("Authorization");
+            String token = null;
+
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+                token = authorizationHeader.substring(7);
+                userName = jwtUtil.extractUsername(token);
+                claims = jwtUtil.extractAllClaims(token);
+            }
+
+            if (userName != null && SecurityContextHolder.getContext().getAuthentication()==null){
+                UserDetails userDetails = service.loadUserByUsername(userName);
+                if (JwtUtil.validateToken(token,userDetails)){
+
+                }
+            }
+        }
+    }
+}
